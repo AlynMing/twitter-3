e97a7bdb00dfa89d4bea16b7376f71041724b2e8
thu285 <thu.hoaido.285@gmail.com>
2020-10-16 19:22:08 -0400

    add char limit and char counting

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var countLabel: UILabel!
+    let charLim = 140
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
     
     @IBAction func onCancel(_ sender: Any) {
@@ -36,6 +43,21 @@ class TweetViewController: UIViewController {
         }
     }
     
+    private func initialCounterValue(text: String?) -> String {
+        if let text = text {
+            return "\(text.utf16.count)/\(charLim)"
+        } else {
+            return "0/\(charLim)"
+        }
+    }
+    
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+        self.countLabel.text = initialCounterValue(text: self.tweetTextView.text)
+        return newText.count <= charLim
+    }
     /*
     // MARK: - Navigation
 
